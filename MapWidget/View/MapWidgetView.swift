5c5434c7dbd066e76263459d4b417087769878e1
Felix Mau <felix.mau@moia.io>
2020-10-15 19:04:44 +0000

    ♻️ :: Extract `MapUserLocationView`

--- MapWidget/View/MapWidgetView.swift
+++ MapWidget/View/MapWidgetView.swift
@@ -9,17 +9,49 @@
 import WidgetKit
 import SwiftUI
 
+struct MapUserLocationView: View {
+    // MARK: - Config
+
+    private enum Config {
+        /// The size of the blue dot reflecting the user location.
+        static let userLocationDotSize: CGFloat = 20
+    }
+
+    // MARK: - Public properties
+
+    let mapImage: Image
+
+    // MARK: - Render
+
+    var body: some View {
+        ZStack {
+            mapImage
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            // The map is centered on the user location, therefore we can simply draw the blue dot in the
+            // center of our view to simulate the user coordinate.
+            Circle()
+                .foregroundColor(Color.blue)
+                .overlay(
+                    Circle()
+                        .stroke(Color.white, lineWidth: 3)
+                )
+                .frame(width: Config.userLocationDotSize,
+                       height: Config.userLocationDotSize)
+        }
+    }
+}
+
 struct MapWidgetView: View {
     // MARK: - Config
 
     private enum Config {
-        ///
+        /// The color to use as a background in case we have an invalid map image.
         static let fallbackColor = Color(red: 225 / 255,
                                          green: 239 / 255,
                                          blue: 210 / 255)
-
-        ///
-        static let userLocationDotSize: CGFloat = 20
     }
 
     // MARK: - Public properties
@@ -29,29 +61,11 @@ struct MapWidgetView: View {
     // MARK: - Render
 
     var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            if let mapImage = entry.mapImage {
-                ZStack {
-                    mapImage
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-
-                    // The map is centered on the user location, therefore we can simply draw the blue dot in the
-                    // center of the view to simulate the user coordinate.
-                    Circle()
-                        .foregroundColor(Color.blue)
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 3)
-                        )
-                        .frame(width: Config.userLocationDotSize,
-                               height: Config.userLocationDotSize)
-                }
-            } else {
-                Rectangle()
-                    .foregroundColor(Config.fallbackColor)
-            }
+        if let mapImage = entry.mapImage {
+            MapUserLocationView(mapImage: mapImage)
+        } else {
+            Rectangle()
+                .foregroundColor(Config.fallbackColor)
         }
     }
 }
