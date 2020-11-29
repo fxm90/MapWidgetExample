66218be9d0f1c74efc5a51195d25c6f772d57af6
Felix Mau <felix.mau@moia.io>
2020-11-29 10:49:34 +0100

    ♻️ :: Fix warning

--- MapWidgetExampleTests/MapWidgetExampleTests.swift
+++ MapWidgetExampleTests/MapWidgetExampleTests.swift
@@ -7,14 +7,15 @@
 //
 
 import XCTest
+
 @testable import MapWidgetExample
 
 class MapWidgetExampleTests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
     }
 }
